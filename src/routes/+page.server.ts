ca54432031101391b44ed9acb4026a1cc7100bc8
Manuel Sanchez <manuel.sanchez@zeit.de>
2024-08-09 15:22:55 +0200

    feat: add possibility of reading csv

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,17 @@
-import { fail, message, setError, superValidate } from 'sveltekit-superforms';
+import type { Actions } from '@sveltejs/kit';
+import { fail, message, setError, superValidate, withFiles } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { z } from 'zod';
 
 const schema = z.object({
   name: z.string().default('Paco'),
-  email: z.string().email().default('paco.the.dog@zeit.de'),
-  image: z
+  email: z.string().email().default("manuel@zeit.de"),
+  // image: z
+  //   .instanceof(File, { message: 'Please upload a file.'})
+  //   .refine((f) => f.size < 100_000, 'Max 100 kB upload size.'),
+  csv: z
     .instanceof(File, { message: 'Please upload a file.'})
-    .refine((f) => f.size < 100_000, 'Max 100 kB upload size.')
+    .refine((f) => f.size < 100_000, 'Max 100 kB upload size.'),
 });
 
 export const load = (async () => {
@@ -16,23 +20,20 @@ export const load = (async () => {
   return {form}
 });
 
-export const actions = {
+export const actions: Actions = {
+  
   default: async ({ request }) => {
-    // Data is posted, so form.errors will be populated
-    const form = await superValidate(request, zod(schema));
+		const form = await superValidate(request, zod(schema));
 
-    if (!form.valid) {
-      return fail(400, { form });
-    }
+		if (!form.valid) return fail(400, withFiles({ form }));
 
     if (form.data.name !== 'Frieder') {
       return setError(form, 'name', 'The name is not Frieder...');
     }
 
-    // return message(form, 'You have done everything amazingly!');
-
-
-    // Unless we turn them off (which is rare in form actions)
-    // const form2 = await superValidate(request, zod(schema), { errors: false });
-  }
+    console.log("this is the form", form);
+    // save to db
+    
+		return withFiles({ form })
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,86 @@
 	import SuperDebug, { fileProxy } from 'sveltekit-superforms';
 	import type { PageData } from './$types';
 	import { superForm } from 'sveltekit-superforms';
-	import { enhance } from '$app/forms';
+	import Papa from 'papaparse';
+	let { data } = $props<{ data: PageData }>();
+	let IS_DEBUG = false;
 
-  let { data } = $props<{ data: PageData }>();
+	const { form, message, constraints, errors, enhance } = superForm(data.form, {
+		resetForm: false,
+		validators: false,
+		SPA: true,
+		onChange(event) {
+			if (IS_DEBUG) {
+				if (event.target) {
+					// Form input event
+					console.log(event.path, 'was changed with', event.target, 'in form', event.formElement);
+				} else {
+					// Programmatic event
+					console.log('Fields updated:', event.paths);
+				}
+			}
+		},
+		onUpdate({ form }) {
+			if (form.valid) {
+				console.log('form is valid');
+				console.log('form:', form);
+				console.log(form.data.csv);
+				Papa.parse(form.data.csv, {
+					header: true,
+					complete: function (results) {
+						console.log('we have parsed the csv');
+						console.log(results.data);
+					}
+				});
 
-  const { form, constraints, errors } = superForm(data.form);
-
-  const file = fileProxy(form, 'image')
-
+				console.log('we are sending to the database the following:');
+			}
+		},
+		onUpdated({ form }) {
+			if (form.valid) {
+				// Successful post! Do some more client-side stuff,
+				// like showing a toast notification.
+				console.log('toastchen here!');
+			}
+		}
+	});
 </script>
 
 <SuperDebug data={$form} />
 
+{#if $message}<h1>{$message}</h1>{/if}
+
 <form method="POST" enctype="multipart/form-data" use:enhance>
 	<label for="name">Names</label>
-	<input type="text" name="name" aria-invalid={$errors.name ? 'true' : undefined} bind:value={$form.name} />
+	<input
+		type="text"
+		name="name"
+		aria-invalid={$errors.name ? 'true' : undefined}
+		bind:value={$form.name}
+	/>
 	{#if $errors.name}<span style="color: red;" class="invalid">{$errors.name}</span>{/if}
+
+	<!-- <label for="image">
+		Upload one image, max 100 Kb:
+		<input type="file" name="image" accept="image/png, image/jpeg" bind:files={$file} />
+	</label>
+	{#if $errors.image}<span style="color: red;">{$errors.image}</span>{/if} -->
 
 	<label for="email">E-mail</label>
 	<input type="email" name="email" bind:value={$form.email} {...$constraints.email} />
 
-	<input
-    type="file"
-    name="image"
-    accept="image/png, image/jpeg"
-    bind:files={$file}
-  />
-  {#if $errors.image}<span style="color: red;">{$errors.image}</span>{/if}
+	<label for="csv">
+		Upload one CSV, max 100 Kb:
+		<input
+			type="file"
+			name="csv"
+			accept=".csv"
+			oninput={(e) => ($form.csv = e.currentTarget.files?.item(0) as File)}
+		/>
+	</label>
+	{#if $errors.csv}<span style="color: red;">{$errors.csv}</span>{/if}
 
 	<div><button>Submit</button></div>
 </form>
+
+
