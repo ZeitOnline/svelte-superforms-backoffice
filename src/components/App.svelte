9092fb60b5cf3a5b2eacb42567f68702b1bb9595
Manuel Sanchez <manuel.sanchez@zeit.de>
2024-08-22 10:54:58 +0200

    test: add some initial tests (app and pagination)

---
+++ src/components/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import DashboardView from '$views/DashboardView.svelte';
+	import DeleteGameView from '$views/DeleteGameView.svelte';
+	import EditGameView from '$views/EditGameView.svelte';
+	import NewGameView from '$views/NewGameView.svelte';
+	import type { PageData } from '../routes/$types';
+	import { viewStateStore, type ViewStateStore } from '../stores/view-state-store.svelte';
+	import Header from './Header.svelte';
+
+	let store: ViewStateStore = viewStateStore('dashboard');
+	let { data }: { data: PageData } = $props();
+</script>
+
+<Header />
+
+{#if store.view == 'new-game'}
+	<NewGameView {store} {data} />
+{:else if store.view == 'dashboard'}
+	<DashboardView {store} games={data.games} />
+{:else if store.view == 'edit-game'}
+	<EditGameView {store} games={data.games} />
+{:else if store.view == 'delete-game'}
+	<DeleteGameView {store} games={data.games} />
+{:else}
+	<DashboardView {store} games={data.games} />
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import Header from '$components/Header.svelte';
-	import NewGameView from '$views/NewGameView.svelte';
-	import DashboardView from '$views/DashboardView.svelte';
-	import EditGameView from '$views/EditGameView.svelte';
-	import DeleteGameView from '$views/DeleteGameView.svelte';
-	import {viewStateStore, type ViewStateStore} from '../stores/view-state-store.svelte';
+	import App from '$components/App.svelte';
 
 	let { data } = $props<{ data: PageData }>();
 
-	let store: ViewStateStore = viewStateStore("dashboard")
 </script>
 
-<Header />
-
-{#if store.view == 'new-game'}
-	<NewGameView {store} {data} />
-{:else if store.view == 'dashboard'}
-	<DashboardView {store} games={data.games} />
-{:else if store.view == 'edit-game'}
-	<EditGameView {store} games={data.games} />
-{:else if store.view == 'delete-game'}
-	<DeleteGameView {store} games={data.games} />
-{:else}
-	<DashboardView {store} games={data.games} />
-{/if}
+<App {data} />
