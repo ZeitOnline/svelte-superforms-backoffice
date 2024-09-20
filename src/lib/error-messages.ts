b8bb2abb4d6fc775c2223c177d80172145fc9c20
fdiegner <frieder.diegner@zeit.de>
2024-09-20 18:38:15 +0200

    fix(validation): add missing validation and error messages in game schema
    Refs: ZO-6269

--- src/lib/error-messages.ts
+++ src/lib/error-messages.ts
@@ -1,3 +1,5 @@
+import { Orientation } from "$types";
+
 export const ERRORS = {
     CSV: {
         NO_FILE: 'Bitte laden sie eine CSV hoch.',
@@ -6,6 +8,7 @@ export const ERRORS = {
         NUMBER_OF_COLUMNS: 'Die CSV Datei muss genau 7 Spalten haben.',
         EMPTY: 'Die CSV Datei darf nicht leer sein.',
     },
+    EMPTY: 'Darf nicht leer sein.',
     GAME: {
         NAME: {
             EMPTY: 'Der Name darf nicht leer sein.',
@@ -15,14 +18,14 @@ export const ERRORS = {
             EMPTY: 'Das Veröffentlichungsdatum darf nicht leer sein.',
         },
         QUESTIONS: {
-            NUMBER: {
-                MIN: 'Die Nummer muss größer als 0 sein.',
+            NR: {
+                MIN: 'Muss größer als 0 sein.',
             },
             QUESTION: 'Die Frage darf nicht leer sein.',
             ANSWER: 'Die Antwort darf nicht leer sein.',
             XC: 'Die X-Koordinate darf nicht leer sein.',
             YC: 'Die Y-Koordinate darf nicht leer sein.',
-            DIRECTION: 'Die Richtung darf nicht leer sein.',
+            DIRECTION: `Die Richtung muss '${Orientation.HORIZONTAL}' oder '${Orientation.VERTICAL}' sein.`,
             DESCRIPTION: 'Die Beschreibung darf nicht leer sein.',
         }
     },
--- src/schemas/generate-game.ts
+++ src/schemas/generate-game.ts
@@ -3,30 +3,31 @@ import { Orientation } from "$types";
 import { z } from "zod";
 
 export const generateGameSchema = z.object({
-    csv: z
-        .instanceof(File, { message: ERRORS.CSV.NO_FILE })
-        .refine((f) => f.size < 100_000, ERRORS.CSV.SIZE)
-        .refine((f) => f.type === 'text/csv', ERRORS.CSV.TYPE),
+	csv: z
+		.instanceof(File, { message: ERRORS.CSV.NO_FILE })
+		.refine((f) => f.size < 100_000, ERRORS.CSV.SIZE)
+		.refine((f) => f.type === 'text/csv', ERRORS.CSV.TYPE)
 });
 
 export const saveGameSchema = z.object({
-    nr: z.number()
-        .min(1, { message: ERRORS.GAME.QUESTIONS.NUMBER.MIN }),
-    question: z.string().min(1),
-    answer: z.string(),
-    xc: z.number(),
-    yc: z.number(),
-    direction: z.nativeEnum(Orientation),
-    description: z.string(),
+	nr: z.number({ message: ERRORS.EMPTY }).min(1, { message: ERRORS.GAME.QUESTIONS.NR.MIN }),
+	question: z.string().min(1, { message: ERRORS.GAME.QUESTIONS.QUESTION }),
+	answer: z.string().min(1, { message: ERRORS.GAME.QUESTIONS.ANSWER }),
+	xc: z.number({ message: ERRORS.EMPTY}),
+	yc: z.number({ message: ERRORS.EMPTY }),
+	direction: z
+		.nativeEnum(Orientation, { message: ERRORS.GAME.QUESTIONS.DIRECTION })
+		.default(Orientation.HORIZONTAL),
+	description: z.string().min(1, { message: ERRORS.GAME.QUESTIONS.DESCRIPTION })
 });
 
 export const saveGameArraySchema = z.array(saveGameSchema);
 
 export const saveGameFormSchema = z.object({
-    name: z.string().trim().min(1, {
-        message: ERRORS.GAME.NAME.EMPTY
-    }),
-    release_date: z.string().date().min(1),
-    published: z.boolean().default(false),
-    questions: saveGameArraySchema
+	name: z.string().trim().min(1, {
+		message: ERRORS.GAME.NAME.EMPTY
+	}),
+	release_date: z.string().date().min(1, { message: ERRORS.GAME.RELEASE_DATE.EMPTY }),
+	published: z.boolean().default(false),
+	questions: saveGameArraySchema
 });
