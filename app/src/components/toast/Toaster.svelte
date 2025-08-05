e609e6753fd71bfe216bb82bc37e369528d83cee
Manuel Sanchez <manuel.sanchez@zeit.de>
2025-08-05 11:48:52 +0200

    feat: implement toast notification system with ToastUnit and Toaster components

---
+++ app/src/components/toast/Toaster.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { getToastState } from '$lib/toast-state.svelte';
+	import Toast from './ToastUnit.svelte';
+
+	const toastState = getToastState();
+</script>
+
+<div class="toaster">
+	{#each toastState.toasts as toast, index}
+		<Toast {toast} total={toastState.toasts.length} {index} />
+	{/each}
+</div>
+
+<style>
+	.toaster {
+		position: fixed;
+		right: 3rem;
+		bottom: 1.5rem;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		pointer-events: none;
+	}
+
+	.toaster :global(.toast) {
+		pointer-events: auto;
+		/* margin-bottom: -2.5rem; Create overlapping effect, TODO: let's talk about thissss */
+	}
+
+	.toaster :global(.toast:first-child) {
+		margin-bottom: 0;
+	}
+</style>
