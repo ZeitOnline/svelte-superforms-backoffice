a1416f3d484b98b8442fbc413ca01c9c2c3abf12
fdiegner <frieder.diegner@zeit.de>
2025-11-24 16:28:29 +0100

    revert: revert base path from /backoffice to /
    base path is managed by the deployment itself, so the app only has to value this when routing

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -19,7 +19,7 @@ const config = {
       $config: './src/config',
     },
     paths: {
-			base: '/backoffice',
+			base: '',
 		},
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
