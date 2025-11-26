299895114b33696e3f0ace5ec3d9e8d75b06edc6
fdiegner <frieder.diegner@zeit.de>
2025-11-26 14:59:47 +0100

    feat(api): use simplified api url with proxy approach in spiele-deployment

--- app/src/lib/server/runtime-config.ts
+++ app/src/lib/server/runtime-config.ts
@@ -1,4 +1,3 @@
-import { env } from '$env/dynamic/private';
 import type { GameType } from '$types';
 import { CONFIG_GAMES } from '../../config/games.config';
 
@@ -6,12 +5,6 @@ import { CONFIG_GAMES } from '../../config/games.config';
  * Reads from private environment variables, so has to be server-side only.
  */
 export function getApiBaseUrl(gameName: GameType): string {
-  if (env?.ZON_ENVIRONMENT === 'staging') {
-    return `https://spiele.staging.zeit.de${CONFIG_GAMES[gameName].apiBase}`;
-  } else if (env?.ZON_ENVIRONMENT === 'production') {
-    return `https://spiele.zeit.de${CONFIG_GAMES[gameName].apiBase}`;
-  }
-
-  // Development fallback
-  return CONFIG_GAMES[gameName].apiBase;
+  const basePath = '/backoffice';
+  return `${basePath}${CONFIG_GAMES[gameName].apiBase}`;
 }
