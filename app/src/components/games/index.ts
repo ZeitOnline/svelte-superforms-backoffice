545080c7109f0f60bb6b71b962ee0fe718ad31ca
Manuel Sanchez <manuel.sanchez@zeit.de>
2026-02-13 14:15:01 +0100

    feat: add wortgeflecht logo, route and mock config

--- app/src/components/games/index.ts
+++ app/src/components/games/index.ts
@@ -1,3 +1,4 @@
 export { default as EckchenLogo } from './eckchen/EckchenLogo.svelte';
 export { default as WortigerLogo } from './wortiger/WortigerLogo.svelte';
 export { default as SpellingBeeLogo } from './spelling-bee/SpellingBeeLogo.svelte';
+export { default as WortgeflechtLogo } from './wortgeflecht/WortgeflechtLogo.svelte';
---
+++ app/src/routes/wortgeflecht/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { DataProps } from '$types';
+  import App from '$components/App.svelte';
+  import Header from '$components/Header.svelte';
+
+  let { data } = $props();
+</script>
+
+<Header gameName="wortgeflecht" />
+<App data={data as DataProps} />
