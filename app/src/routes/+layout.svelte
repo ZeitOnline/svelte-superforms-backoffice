1f44393d0042b4b3190227594810e4749884cd86
fdiegner <frieder.diegner@zeit.de>
2026-02-23 15:43:59 +0100

    feat: wrap application in oidc login wrapper
    Refs: ENG-635

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,13 +3,16 @@
   import BreakpointsDebug from '$components/BreakpointsDebug.svelte';
   import Toaster from '$components/toast/Toaster.svelte';
   import { setToastState } from '$lib/toast-state.svelte';
+  import OidcLogin from '$components/OidcLogin.svelte';
 
   setToastState();
 </script>
 
-<div class="max-w-5xl w-full mx-auto px-5 md:px-12 py-24">
-  <slot />
-</div>
+<OidcLogin>
+  <div class="max-w-5xl w-full mx-auto px-5 md:px-12 py-24">
+    <slot />
+  </div>
+</OidcLogin>
 
 <BreakpointsDebug />
 <Toaster />
