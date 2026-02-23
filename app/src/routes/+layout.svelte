56e4e2eef7dd90da599cc809cb770a7a05c959a3
fdiegner <frieder.diegner@zeit.de>
2026-02-23 15:56:47 +0100

    refactor: use svelte 5 snippet pattern in layout.svelte
    Refs: ENG-635

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,16 +1,18 @@
-<script>
+<script lang="ts">
   import '../app.css';
   import BreakpointsDebug from '$components/BreakpointsDebug.svelte';
   import Toaster from '$components/toast/Toaster.svelte';
   import { setToastState } from '$lib/toast-state.svelte';
   import OidcLogin from '$components/OidcLogin.svelte';
 
+  let { children }: { children: () => ReturnType<import('svelte').Snippet> } = $props();
+
   setToastState();
 </script>
 
 <OidcLogin>
   <div class="max-w-5xl w-full mx-auto px-5 md:px-12 py-24">
-    <slot />
+    {@render children()}
   </div>
 </OidcLogin>
 
